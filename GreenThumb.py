f45afffb3dcae466b4cdf2ac2f97660da4797310
sseshan <srini@cs.cmu.edu>
2019-12-24 10:31:20 -0500

    initial upload

---
+++ GreenThumb.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python3
+# Copyright 2019 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+# 
+# You may not use this file except in compliance with the terms and conditions 
+# set forth in the accompanying LICENSE.TXT file.
+#
+# THESE MATERIALS ARE PROVIDED ON AN "AS IS" BASIS. AMAZON SPECIFICALLY DISCLAIMS, WITH 
+# RESPECT TO THESE MATERIALS, ALL WARRANTIES, EXPRESS, IMPLIED, OR STATUTORY, INCLUDING 
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT.
+
+import os
+import sys
+import time
+import logging
+import json
+import random
+import threading
+
+from enum import Enum
+from agt import AlexaGadget
+
+from ev3dev2.led import Leds
+from ev3dev2.sound import Sound
+from ev3dev2.motor import OUTPUT_D, SpeedPercent, LargeMotor
+
+# Set the logging level to INFO to see messages from AlexaGadget
+logging.basicConfig(level=logging.INFO, stream=sys.stdout, format='%(message)s')
+logging.getLogger().addHandler(logging.StreamHandler(sys.stderr))
+logger = logging.getLogger(__name__)
+
+
+class Light(Enum):
+    """
+    The list of preset commands and their invocation variation.
+    These variations correspond to the skill slot values.
+    """
+    ON = ['on', 'bright']
+    OFF = ['off', 'dark']
+
+
+
+class MindstormsGadget(AlexaGadget):
+    """
+    A Mindstorms gadget that performs movement based on voice commands.
+    Two types of commands are supported, directional movement and preset.
+    """
+
+    def __init__(self):
+        """
+        Performs Alexa Gadget initialization routines and ev3dev resource allocation.
+        """
+        super().__init__()
+
+        # Gadget state
+        self.patrol_mode = False
+
+        # Ev3dev initialization
+        self.leds = Leds()
+        self.sound = Sound()
+        self.switch = LargeMotor(OUTPUT_D)
+
+    def on_connected(self, device_addr):
+        """
+        Gadget connected to the paired Echo device.
+        :param device_addr: the address of the device we connected to
+        """
+        self.leds.set_color("LEFT", "GREEN")
+        self.leds.set_color("RIGHT", "GREEN")
+        logger.info("{} connected to Echo device".format(self.friendly_name))
+
+    def on_disconnected(self, device_addr):
+        """
+        Gadget disconnected from the paired Echo device.
+        :param device_addr: the address of the device we disconnected from
+        """
+        self.leds.set_color("LEFT", "BLACK")
+        self.leds.set_color("RIGHT", "BLACK")
+        logger.info("{} disconnected from Echo device".format(self.friendly_name))
+
+    def on_custom_mindstorms_gadget_control(self, directive):
+        """
+        Handles the Custom.Mindstorms.Gadget control directive.
+        :param directive: the custom directive with the matching namespace and name
+        """
+        try:
+            payload = json.loads(directive.payload.decode("utf-8"))
+            print("Control payload: {}".format(payload), file=sys.stderr)
+            control_type = payload["type"]
+
+            if control_type == "light":
+                # Expected params: [action]
+                self._activate(payload["action"])
+
+        except KeyError:
+            print("Missing expected parameters: {}".format(directive), file=sys.stderr)
+
+    def _activate(self, command):
+        """
+        Handles preset commands.
+        :param command: the preset command
+        :param speed: the speed if applicable
+        """
+        print("Light command: ({})".format(command), file=sys.stderr)
+        if command in Light.ON.value:
+            self.switch.on_for_degrees(SpeedPercent(20), 50)
+
+        if command in Light.OFF.value:
+            self.switch.on_for_degrees(SpeedPercent(20), -50)
+
+
+
+if __name__ == '__main__':
+
+    gadget = MindstormsGadget()
+
+    # Set LCD font and turn off blinking LEDs
+    os.system('setfont Lat7-Terminus12x6')
+    gadget.leds.set_color("LEFT", "BLACK")
+    gadget.leds.set_color("RIGHT", "BLACK")
+
+    # Startup sequence
+    gadget.sound.play_song((('C4', 'e'), ('D4', 'e'), ('E5', 'q')))
+    gadget.leds.set_color("LEFT", "GREEN")
+    gadget.leds.set_color("RIGHT", "GREEN")
+
+    # Gadget main entry point
+    gadget.main()
+
+    # Shutdown sequence
+    gadget.sound.play_song((('E5', 'e'), ('C4', 'e')))
+    gadget.leds.set_color("LEFT", "BLACK")
+    gadget.leds.set_color("RIGHT", "BLACK")
---
+++ skill-nodejs/lambda/index.js
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2019 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+ *
+ * You may not use this file except in compliance with the terms and conditions 
+ * set forth in the accompanying LICENSE.TXT file.
+ *
+ * THESE MATERIALS ARE PROVIDED ON AN "AS IS" BASIS. AMAZON SPECIFICALLY DISCLAIMS, WITH 
+ * RESPECT TO THESE MATERIALS, ALL WARRANTIES, EXPRESS, IMPLIED, OR STATUTORY, INCLUDING 
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT.
+*/
+
+// This sample demonstrates sending directives to an Echo connected gadget from an Alexa skill
+// using the Alexa Skills Kit SDK (v2). Please visit https://alexa.design/cookbook for additional
+// examples on implementing slots, dialog management, session persistence, api calls, and more.
+
+const Alexa = require('ask-sdk-core');
+const Util = require('./util');
+const Common = require('./common');
+
+// The namespace of the custom directive to be sent by this skill
+const NAMESPACE = 'Custom.Mindstorms.Gadget';
+
+// The name of the custom directive to be sent this skill
+const NAME_CONTROL = 'control';
+
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
+    },
+    handle: async function(handlerInput) {
+
+        let request = handlerInput.requestEnvelope;
+        let { apiEndpoint, apiAccessToken } = request.context.System;
+        let apiResponse = await Util.getConnectedEndpoints(apiEndpoint, apiAccessToken);
+        if ((apiResponse.endpoints || []).length === 0) {
+            return handlerInput.responseBuilder
+            .speak(`I couldn't find an EV3 Brick connected to this Echo device. Please check to make sure your EV3 Brick is connected, and try again.`)
+            .getResponse();
+        }
+
+        // Store the gadget endpointId to be used in this skill session
+        let endpointId = apiResponse.endpoints[0].endpointId || [];
+        Util.putSessionAttribute(handlerInput, 'endpointId', endpointId);
+
+        return handlerInput.responseBuilder
+            .speak("Welcome, you can start issuing plant caretaking commands")
+            .reprompt("Awaiting commands")
+            .getResponse();
+    }
+};
+
+// Add the speed value to the session attribute.
+// This allows other intent handler to use the specified speed value
+// without asking the user for input.
+const LightIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'LightIntent';
+    },
+    handle: function (handlerInput) {
+        const request = handlerInput.requestEnvelope;
+        const action = Alexa.getSlotValue(request, 'LightStatus');
+        let speechOutput = 'Turning light ' + action
+
+        // Get data from session attribute
+        const attributesManager = handlerInput.attributesManager;
+        const endpointId = attributesManager.getSessionAttributes().endpointId || [];
+
+        // Construct the directive with the payload containing the move parameters
+        const directive = Util.build(endpointId, NAMESPACE, NAME_CONTROL,
+            {
+                type: 'light',
+                action: action,
+            });
+
+        return handlerInput.responseBuilder
+            .speak(speechOutput)
+            .reprompt("awaiting command")
+            .addDirective(directive)
+            .getResponse();
+
+
+    }
+};
+
+// The SkillBuilder acts as the entry point for your skill, routing all request and response
+// payloads to the handlers above. Make sure any new handlers or interceptors you've
+// defined are included below. The order matters - they're processed top to bottom.
+exports.handler = Alexa.SkillBuilders.custom()
+    .addRequestHandlers(
+        LaunchRequestHandler,
+        LightIntentHandler,
+        Common.HelpIntentHandler,
+        Common.CancelAndStopIntentHandler,
+        Common.SessionEndedRequestHandler,
+        Common.IntentReflectorHandler, // make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
+    )
+    .addRequestInterceptors(Common.RequestInterceptor)
+    .addErrorHandlers(
+        Common.ErrorHandler,
+    )
+    .lambda();
